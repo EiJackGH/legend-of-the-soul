72d4caf8c58841f9794c1f696f998eecba2c3bb5
EiJackGH <rahmatnaim.48@gmail.com>
2026-08-07 19:43:58 +0800

    Create player.java

---
+++ src/main/java/com/legendofthesoul/model/player.java
@@ -0,0 +1,38 @@
+package com.legendofthesoul.model;
+
+public class Player extends Entity {
+    private int souls;
+    private int level;
+
+    public Player(String name) {
+        super(name, 100, 20);
+        this.souls = 0;
+        this.level = 1;
+    }
+
+    public void addSouls(int amount) {
+        this.souls += amount;
+        checkLevelUp();
+    }
+
+    private void checkLevelUp() {
+        int requiredSouls = level * 50;
+        if (souls >= requiredSouls) {
+            level++;
+            souls -= requiredSouls;
+            maxHealth += 25;
+            health = maxHealth;
+            attackPower += 8;
+            System.out.printf("%n🌟 SOUL ASCENSION! You reached Level %d! Max HP and Attack Increased.%n", level);
+        }
+    }
+
+    public void heal() {
+        int healAmount = 30;
+        this.health = Math.min(maxHealth, health + healAmount);
+        System.out.printf("✨ You channel energy and restore %d HP. Current HP: %d/%d%n", healAmount, health, maxHealth);
+    }
+
+    public int getSouls() { return souls; }
+    public int getLevel() { return level; }
+}
